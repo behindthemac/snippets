1cecb7533c9674be4178a329507dad61edac0bfb
Keisuke Miura <keisuke@g.ecc.u-tokyo.ac.jp>
2021-04-07 10:22:48 +0900

    generate all possible permutations of an array

---
+++ permutations.py
@@ -0,0 +1,14 @@
+def permutations(array, permutation=[]):
+    """Generate all possible permutations of an array.
+
+    Argument:
+    array -- array that is permutated
+    """
+    n = len(array)
+    if n > 0:
+        for i in range(n):
+            array_copy = array.copy()
+            element = array_copy.pop(i)
+            yield from permutations(array_copy, permutation + [element])
+    else:
+        yield permutation
