be7e419b0e87d5515ed47bb9ba3643b60e3ead71
Keisuke Miura <keisuke@g.ecc.u-tokyo.ac.jp>
2021-04-24 23:16:42 +0900

    get the indefinite integral of a function

--- calculus.py
+++ calculus.py
@@ -10,3 +10,7 @@ def definite_integral(f, a, b, dx):
     n = int((b - a) / dx)
     x = [a + dx * i for i in range(n)]
     return sum([f(x_i) for x_i in x]) * dx
+
+
+def indefinite_integral(f, a, dx):
+    return lambda x: definite_integral(f, a, x, dx)
