914d5b6bdcc2f4e009c77c69916aa2569071cda4
Keisuke Miura <keisuke@g.ecc.u-tokyo.ac.jp>
2023-09-03 02:09:58 +0900

    call JSONPlaceholder using an async function

--- json_placeholder.js
+++ json_placeholder.js
@@ -1,3 +1,5 @@
-fetch("https://jsonplaceholder.typicode.com/todos/1")
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+async function fetchJsonPlaceholder() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const json = await response.json();
+  console.log(json);
+}
