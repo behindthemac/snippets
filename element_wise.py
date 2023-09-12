a31c4f9a716ee7d66ec19d12202cf506073facb1
Keisuke Miura <keisuke@g.ecc.u-tokyo.ac.jp>
2023-09-13 07:23:18 +0900

    add an usage example

--- element_wise.py
+++ element_wise.py
@@ -8,3 +8,13 @@ def element_wise(f):
         The function whose operation made element-wise
     """
     return lambda *args: [f(*arg) for arg in zip(*args)]
+
+
+@element_wise
+def add(a, b):
+    return a + b
+
+a = [0, 2, 4, 6, 8]
+b = [1, 3, 5, 7, 9]
+c = add(a, b)
+print(c)
