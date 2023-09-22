4aeb2cc8c8d8472729a26a2320ed15a7df68261c
Keisuke Miura <keisuke@g.ecc.u-tokyo.ac.jp>
2023-09-23 08:08:10 +0900

    implement the Euclidean algorithm iteratively

--- euclidean_algorithm.py
+++ euclidean_algorithm.py
@@ -12,3 +12,19 @@ def euclidean_algorithm_recursive(a, b):
     if b != 0:
         return euclidean_algorithm(b, a % b)
     return a
+
+
+# iterative
+def euclidean_algorithm_iterative(a, b):
+    """Returns the greatest common divisor of two integers.
+
+    Args:
+        a: A positive integer
+        b: A positive integer
+
+    Returns:
+        The greatest common divisor of the two integers.
+    """
+    while b != 0:
+        a, b = b, a % b
+    return a
