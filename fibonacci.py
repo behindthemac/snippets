7d195f0e9bd9a976e5483ed3968c2b14352275f6
Keisuke Miura <keisuke@g.ecc.u-tokyo.ac.jp>
2023-09-29 08:49:09 +0900

    compute a Fibonacci number iteratively

--- fibonacci.py
+++ fibonacci.py
@@ -15,3 +15,20 @@ def fibonacci_recursive(n, memo=None):
         memo[n] = fibonacci_recursive(n - 2, memo) + fibonacci_recursive(n - 1, memo)
 
     return memo[n]
+
+
+# iterative
+def fibonacci_iterative(n):
+    """Returns the n-th Fibonacci number.
+
+    Args:
+        n: The index of the Fibonacci number
+
+    Returns:
+        The n-th Fibonacci number
+    """
+    a = 0
+    b = 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
