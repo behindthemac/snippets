a9d133168b73817a7c8d1b5713973de91c5779d5
Keisuke Miura <keisuke@g.ecc.u-tokyo.ac.jp>
2023-09-28 16:53:48 +0900

    compute the factorial iteratively

--- factorial.py
+++ factorial.py
@@ -11,3 +11,19 @@ def factorial_recursive(n):
     if n > 0:
         return n * factorial_recursive(n - 1)
     return 1
+
+
+# iterative
+import functools
+
+
+def factorial_iterative(n):
+    """Returns the factorial of a non-negative integer.
+
+    Args:
+        n: A non-negative integer
+
+    Returns:
+        The factorial of the non-negative integer
+    """
+    return functools.reduce(lambda a, b: a * b, [i for i in range(1, n + 1)], 1)
